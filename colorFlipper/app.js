9ca19ec913376c60406358a05ad6f1592a9b291a
Shukrullah shirzad <shukrullah_shirzad@yahoo.com>
2022-02-14 14:29:29 +0430

    Update app.js

--- colorFlipper/app.js
+++ colorFlipper/app.js
@@ -1,1 +1,12 @@
-
+const body = document.querySelector('#body');
+const color = document.querySelector('#color');
+const button = document.querySelector('#btn');
+const colors = ['red','green', 'blue', 'black', 'yellow', 'purple'];
+button.addEventListener("click", (e)=>{
+    const randomNumber = getRandomNumber();
+    body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length)
+}
